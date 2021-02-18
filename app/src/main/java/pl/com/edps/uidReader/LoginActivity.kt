8f057ae344ca1ac33cb5954cfed6aacd3e26ad13
glowinska <anna.glowinska98@gmail.com>
2021-02-18 12:28:20 +0100

    Logging with shared preferences

--- app/src/main/java/pl/com/edps/uidReader/LoginActivity.kt
+++ app/src/main/java/pl/com/edps/uidReader/LoginActivity.kt
@@ -1,63 +1,53 @@
 package pl.com.edps.uidReader
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
-//    private var edUsername = EditText(this)
-//    private var edPassword = EditText(this)
-//    private var btnLogin = Button(this)
-//    private var btnSignUp = Button(this)
-
-    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var edUsername = findViewById<EditText>(R.id.ed_username)
-        var edPassword = findViewById<EditText>(R.id.ed_password)
-        var btnLogin = findViewById<Button>(R.id.btn_login)
-        var btnSignUp = findViewById<Button>(R.id.btn_signup)
-
-        btnSignUp.setOnClickListener {
+        btn_signup.setOnClickListener {
             val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             intent.putExtra("key", "Kotlin")
             startActivity(intent)
         }
 
-        btnLogin.setOnClickListener {
-            var sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-            var strUsername = sharedPreferences.getString("Username", null)
-            var strPassword = sharedPreferences.getString("Password", null)
+        btn_login.setOnClickListener {
+            val sharedPreferences = getSharedPreferences(
+                "com.example.myapp.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+            val strUsername = sharedPreferences.getString("Username", null)
+            val strPassword = sharedPreferences.getString("Password", null)
 
-            var edPassword = edPassword.text.toString()
-            var edUsername = edUsername.text.toString()
-
-            if (edPassword == strPassword) {
-                if (edUsername == strUsername) {
-                    Toast.makeText(this, "Login succeed!", Toast.LENGTH_SHORT)
-                    Log.d("LOGIN","Login succeed!")
+            if (strUsername != null && strUsername.equals(
+                    ed_username.text.toString(),
+                    ignoreCase = true
+                )
+            ) {
+                if (strPassword != null && strPassword.equals(
+                        ed_password.text.toString(),
+                        ignoreCase = true
+                    )
+                ) {
+                    Toast.makeText(this@LoginActivity, "Login Successful", Toast.LENGTH_SHORT)
+                        .show()
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    intent.putExtra("key", "Kotlin")
+                    startActivity(intent)
                 } else {
-                    Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT)
-                    Log.d("LOGIN","Login failed!")
+                    Toast.makeText(this@LoginActivity, "Login Failed", Toast.LENGTH_SHORT)
+                        .show()
                 }
             } else {
-                Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT)
-                Log.d("LOGIN","Login failed!")
+                Toast.makeText(this@LoginActivity, "Login Failed", Toast.LENGTH_SHORT).show()
             }
-
-//            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
-//            intent.putExtra("key", "Kotlin")
-//            startActivity(intent)
         }
-
     }
 }
--- app/src/main/java/pl/com/edps/uidReader/SignUpActivity.kt
+++ app/src/main/java/pl/com/edps/uidReader/SignUpActivity.kt
@@ -1,51 +1,27 @@
 package pl.com.edps.uidReader
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import android.net.wifi.hotspot2.pps.Credential
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUpActivity : AppCompatActivity() {
 
-//    private var edUsername = EditText(this)
-//    private var edPassword = EditText(this)
-//    private var edConfirmPassword = EditText(this)
-//    private var btnCreateUser = Button(this)
-
-
-    @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        var edUsername = findViewById<EditText>(R.id.ed_username)
-        var edPassword = findViewById<EditText>(R.id.ed_password)
-        var edConfirmPassword = findViewById<EditText>(R.id.ed_confirm_password)
-        var btnCreateUser = findViewById<Button>(R.id.btn_create_user)
-
-        btnCreateUser.setOnClickListener {
-
-            var strPassword = edPassword.text.toString()
-            var strConfirmPassword = edConfirmPassword.text.toString()
-            var strUsername = edUsername.text.toString()
-
-            if (strPassword == strConfirmPassword) {
-                var sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-                var editor = sharedPreferences.edit()
-                editor.putString("Password", strPassword)
-                editor.putString("Username", strUsername)
+        btn_create_user.setOnClickListener {
+            if (ed_password.text.toString() == ed_confirm_password.text.toString()) {
+                val sharedPref = getSharedPreferences(
+                    "com.example.myapp.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
+                val editor = sharedPref.edit()
+                editor.putString("Password", ed_password.text.toString())
+                editor.putString("Username", ed_username.text.toString())
                 editor.apply()
-
                 this.finish()
             }
-//            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
-//            intent.putExtra("key", "Kotlin")
-//            startActivity(intent)
         }
     }
 }
